a4a946c7524bd7f7843516c7cf856fad5282e8f0
VenusLoveAlan <whanghao243249386@163.com>
2019-05-31 15:36:10 +0800

    first commit

---
+++ scripts/test_allure_demo.py
@@ -0,0 +1,15 @@
+
+
+class TestAllureDemo:
+
+    def test_login1(self):
+        print('test_login1')
+        assert True
+
+    def test_login2(self):
+        print('test_login2')
+        assert False
+
+    def test_login3(self):
+        print('test_login3')
+        assert True
